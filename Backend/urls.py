87ef74931ed4c05e10c911c94a0499f567423602
Alona s Rajesh <alonasrajesh908@gmail.com>
2026-04-16 14:13:39 +0530

    sixth commit

--- Backend/urls.py
+++ Backend/urls.py
@@ -6,8 +6,13 @@ from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("Medcare API is running 🚀")
 
 urlpatterns = [
+    path('', home),
     path('admin/', admin.site.urls),
     path('api/v3/',include('API.urls')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
